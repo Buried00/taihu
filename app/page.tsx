e3034a05d2a73f9b31d1ebd87cb18b94971efc85
8914866804ll-debug <8914866804ll@gmail.com>
2026-02-04 09:53:08 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
 import Image from "next/image";
 import HeroImage from "./components/hero/heroImage";
 import Main_Menu from "./components/hero/mainMenu";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#1a1a1a]">
+    <main className="min-h-screen bg-[#1a1a1a]"> 
+      <nav>
+        <Main_Menu onOpenReservation={() => setIsResOpen(true)} />
+      </nav>
       <HeroImage />
-      <Main_Menu />
     </main>
   );
 }
