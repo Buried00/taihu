15ab633e544a4e52af972ea2a9b190adb3a4ddb0
8914866804ll-debug <8914866804ll@gmail.com>
2026-02-04 09:50:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroImage from "./components/hero/heroImage";
+import Main_Menu from "./components/hero/mainMenu";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1a1a1a]">
       <HeroImage />
+      <Main_Menu />
     </main>
   );
 }
